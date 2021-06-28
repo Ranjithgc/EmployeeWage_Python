638ed1af36fc9b46ff1cae031211ba324bddc1f1
Ranjithgc <Ranjith.amca.16@acharya.ac.in>
2021-06-29 03:28:10 +0530

    [Ranjithgc] Add: Check Employee Present or Absent

--- EmployeeWage.py
+++ EmployeeWage.py
@@ -5,4 +5,23 @@
 @Last Modified Time: 2021-06-27
 @Title: Employee Wage Computation Program
 """
+import random
+
 print("Welcome to Employee Wage Computation problem")
+
+#Uc1: Check Employee is present or absent
+
+def employee_attendance():
+    """
+    Description:
+        This function will check if employee is present or absent
+    """
+    present = 1
+
+    emp_Check = random.randint(0,2)
+    if emp_Check == present:
+        print("Employee is present")
+    else:
+        print("Employee is absent")
+
+employee_attendance()

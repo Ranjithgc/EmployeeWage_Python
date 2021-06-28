47bd92c56070f1199afdb8c809816160330f3c44
Ranjithgc <Ranjith.amca.16@acharya.ac.in>
2021-06-29 03:35:02 +0530

    [Ranjithgc] Add: Calculate Daily Employee Wage

--- EmployeeWage.py
+++ EmployeeWage.py
@@ -5,4 +5,30 @@
 @Last Modified Time: 2021-06-27
 @Title: Employee Wage Computation Program
 """
+import random
+
 print("Welcome to Employee Wage Computation problem")
+
+#Uc2: Calculating Daily Employee Wage
+
+def employee_attendance():
+    """
+    Description:
+        This function will calculate Daily Employee Wage
+    """
+    # constants
+    PRESENT = 1
+    # instance variable
+    wage_per_hr = 20
+
+    emp_Check = random.randint(0,2)
+    if emp_Check == PRESENT:
+        print("Employee is present")
+        wrk_Hr = 8
+    else:
+        print("Employee is absent")
+        wrk_Hr = 0
+    employee_Wage = wage_per_hr * wrk_Hr 
+    print("Employee Wage is - ", employee_Wage)
+
+employee_attendance()

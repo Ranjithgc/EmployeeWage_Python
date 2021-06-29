98c4d2c2429fea1de58638b1a086bc96cbd139c8
Ranjithgc <Ranjith.amca.16@acharya.ac.in>
2021-06-28 20:05:16 -0400

    [Ranjithgc] Add: Displaying Message

---
+++ EmployeeWage.py
@@ -0,0 +1,8 @@
+"""
+@Author: Ranjith G C
+@Date: 2021-06-27
+@Last Modified by: Ranjith G C
+@Last Modified Time: 2021-06-27
+@Title: Employee Wage Computation Program
+"""
+print("Welcome to Employee Wage Computation problem")
